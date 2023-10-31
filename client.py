790b1289b98d5252b4f37d7b0f3460042deafccf
Pizon <thanhhangdang16@gmail.com>
2023-10-31 21:45:05 +0700

    a

---
+++ client.py
@@ -0,0 +1,190 @@
+import socket
+import threading #chạy đa luồn
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import json
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+# Define global variables to hold the entry fields
+e1 = None
+e2 = None
+entry_filenameDownload = None
+entry_filenameUpload = None
+entry_file_path = None
+
+CLIENTSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+CLIENTSOCKET.connect((HOST, PORT))
+
+#đăng nhập
+def isLogin(user, password):
+    data = {
+        'username': user,
+        'password': password,
+        'method': 'login'
+    }
+    json_data = json.dumps(data)
+    print(json_data)
+    CLIENTSOCKET.send(json_data.encode())
+
+def loginResponse():
+    res = CLIENTSOCKET.recv(1024).decode()
+    print(res)
+    if res == 'OK':
+        chooseLayout()
+    else:
+        CLIENTSOCKET.close()
+        receiveUpLoadResponseFromServer("Login failed. Please try again.")
+
+    
+
+#Gửi filename lên server để tìm address client
+def sendFilenameToServerToFind(filename):
+    #lấy tên file name từ trường nhập liệu
+    data = {
+        'filename' : filename,
+        'method' : 'download',  
+    }
+    json_data = json.dumps(data)  # Convert dictionary to JSON string
+    CLIENTSOCKET.send(json_data.encode())
+
+    receiveAddressFromServer()
+
+#nhận respon
+def receiveAddressFromServer():
+    address = CLIENTSOCKET.recv(1024).decode() #nhận về địa chỉ chưa file từ server
+    if address != "False":
+        print("File found at: ", address)
+        #Thực hiện p2p với client(address) chưa file
+
+    else:
+        print(text="Cannot find client having this file")
+
+#gửi data để cập nhật, sửa
+def sendInformationOfFileToServerToSaveToDatabase(filename, path):
+    data = {
+        'filename' : filename,
+        'path' : path,
+        'method' : 'upload',
+    }
+    json_data = json.dumps(data) 
+    CLIENTSOCKET.send(json_data.encode())
+
+    receiveUpLoadResponseFromServer()
+
+#nhận và show kết quả
+def receiveUpLoadResponseFromServer():
+    res = CLIENTSOCKET.recv(1024).decode()
+    res_window = tk.Toplevel()
+    res_window.title("Upload Response")
+    
+    label = tk.Label(res_window, text=res)
+    label.pack()
+    
+    res_window.mainloop()   
+
+def searchButtonClicked(entry_filenameDownload):
+    filename = entry_filenameDownload.get() # Lấy giá trị từ trường nhập liệu
+    sendFilenameToServerToFind(filename)
+
+def uploadButtonClicked(entry_filenameUpload, entry_file_path):
+    filename = entry_filenameUpload.get()
+    path = entry_file_path.get()
+    sendInformationOfFileToServerToSaveToDatabase(filename, path)
+
+def openDownloadWindow(root):
+    root.destroy() #Đóng cửa sổ hiện tại
+    #tạo cửa sổ cho phần download
+    download_window = tk.Tk()
+    download_window.title("Download")
+    download_window.geometry('300x150')
+    #các thành phần và layout cho phần download
+    label_name = tk.Label(download_window, text="Filename to download:",bg='#eb85de')
+    label_name.pack()
+    # Tạo trường nhập liệu
+    entry_filenameDownload = tk.Entry(download_window)
+    entry_filenameDownload.pack()
+    # Tạo nút "Search"
+    search_button = tk.Button(download_window, text="Search File to DownLoad", command=lambda: searchButtonClicked(entry_filenameDownload))
+    search_button.pack()
+    download_window.mainloop()
+
+def openUploadWindow(root):
+    root.destroy()
+    #Tạo cửa sổ cho Upload
+    upload_window = tk.Tk()
+    upload_window.title("Upload")
+    upload_window.geometry('300x150')
+    #Các thành phần và layout cho phần upload
+    label_name_upload = tk.Label(upload_window, text="Filename to upload:",bg='#eb85de')
+    label_name_upload.pack()
+    # Tạo trường nhập liệu
+    entry_filenameUpload = tk.Entry(upload_window)
+    entry_filenameUpload.pack()
+    # Khung chọn file
+    def browse_file():
+        file_path = filedialog.askopenfilename()
+        entry_file_path.delete(0, tk.END)
+        entry_file_path.insert(tk.END, file_path)
+
+    label_file_path = tk.Label(upload_window, text="File Path:")
+    label_file_path.pack()
+
+    entry_file_path = tk.Entry(upload_window)
+    entry_file_path.pack()
+
+    button_browse = tk.Button(upload_window, text="Choose FIle", command=browse_file)
+    button_browse.pack()
+
+    upload_button = tk.Button(upload_window, text="File to Upload", command=lambda: uploadButtonClicked(entry_filenameUpload, entry_file_path))
+    upload_button.pack()
+    
+    upload_window.mainloop()
+
+def openDownloadOrUploadWindow(choice):
+    if choice == "download":
+        openDownloadWindow()
+    elif choice == "upload":
+        openUploadWindow()
+
+
+def chooseLayout():
+    # Tạo giao diện tkinter
+    root = tk.Tk()
+    root.title("File Upload/Download Example")
+    root.geometry('300x150')
+
+    # Tạo nút "Download"
+    download_button = tk.Button(root, text="Download", command=lambda: openDownloadOrUploadWindow("download"))
+    download_button.pack()
+
+    # Tạo nút "Upload"
+    upload_button = tk.Button(root, text="Upload", command=lambda: openDownloadOrUploadWindow("upload"))
+    upload_button.pack()
+    root.mainloop()
+
+def login(e1, e2):
+    username = e1.get()
+    password = e2.get()
+    print(username, password)
+    isLogin(username, password)
+    login_window.destroy()
+
+
+#Tạo giao diện login
+login_window = tk.Tk()
+login_window.title("Please Login")
+login_window.geometry('300x150')
+name = tk.Label(login_window, text = "Name").grid(row = 0, column = 0)
+e1 = tk.Entry(login_window)
+e1.grid(row = 0, column = 1)
+password = tk.Label(login_window, text = "Password").grid(row = 1, column = 0)
+e2 = tk.Entry(login_window)
+e2.grid(row = 1, column = 1)
+submit = Button(login_window, text = "Submit", command=lambda:login(e1, e2)).grid(row = 4, column = 0)
+login_window.mainloop()
+
+
+
---
+++ sever.py
@@ -0,0 +1,100 @@
+import socket
+from os import error #xuất ra lỗi
+import os
+import threading #chạy đa luồn
+import json
+
+#đặt host là IP của máy
+HOST = "127.0.0.1"
+PORT = 8080
+
+#Tạo server socket
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Dùng try-except để bắt sự kiện
+    #Cụ thể dùng socket SERVER kết nối (bind) vào HOST, PORT
+        #Thành công in ra thông báo...
+        #Lỗi in ra mã lỗi
+try:
+    SERVER.bind((HOST,PORT))
+    print(f'* Running on http://{HOST}:{PORT}')
+except socket.error as e:
+    print(f'socket error: {e}')
+    print('socket error: %s' %(e))
+
+
+def _start():
+    SERVER.listen() #lắng nghe yêu cầu từ phía client
+    while True:
+        conn, addr = SERVER.accept() #giá trị trả về accept là kết nối và địa chỉ
+        #Khai báo thread để nhận dữ liệu đa luồng
+        thread = threading.Thread(target=_handle, args=(conn, addr))
+        thread.start()
+
+#nhận dữ liệu khi server chấp nhận kết nối socket
+def _handle(conn, addr):
+    while True:
+        #Dữ liệu nhận được dạng bytes nên decode về string
+        data = conn.recv(1024).decode()
+        #nếu không có data thì thoát khỏi vòng lặp để chạy hàm handle khác
+        if not data: break
+
+        data_dict = json.loads(data)  # Convert JSON string to dictionary
+        res = []
+
+        if data_dict['method'] == 'download':
+            with open('database.json', 'r') as file:
+                database =json.load(file)
+                res = []
+                for item in database:
+                    if item['filename'] == data_dict['filename']:
+                        res.append(item)
+            file.close()
+
+        elif data_dict['method'] == 'upload':
+            with open('database.json', 'r') as file:
+                database = json.load(file)
+                found = False
+                for item in database:
+                    if item['filename'] == data_dict['filename'] :
+                        item['path'] = data_dict['path']
+                        found = True
+                        res = 'Database is updated Successfull!!!'
+                
+                if not found:
+                    new_item = {
+                        "filename" : data_dict['filename'],
+                        "ipaddress" : addr,
+                        'path' : data_dict['path']
+                    }
+                    database.append(new_item)
+                    res = 'Database is added Successfull!!!'
+                        
+            with open('database.json','w') as file:
+                json.dump(database, file)        
+            file.close()                    
+
+        elif data_dict['method'] == 'login':
+            with open('login.json', 'r') as file:
+                user = json.load(file)
+                for item in user:
+                    if item['username'] == data_dict['username'] and item['password'] == data_dict['password']:
+                        res = 'OK'
+                    else:
+                        res = 'Not user'
+            file.close()
+
+        else:
+            print('dkuyum')
+
+        if not isinstance(res, str):
+            res_dict = json.dumps(res)
+        else:
+            res_dict = res
+        #nhận được dữ liệu thì phản hồi về phía client, encode() về lại dạng bytes
+        conn.send(res_dict.encode())
+        #conn.close() # đóng kết nối
+        #break #thoát khỏi vòng lặp để nhận handle khắc
+
+if __name__ == '__main__':
+    _start()
